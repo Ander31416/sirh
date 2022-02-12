88e80064de2206329a9a745a50c651f6b7e96006
Ander31416 <89935076+Ander31416@users.noreply.github.com>
2022-02-12 13:37:31 -0600

    Add files via upload

---
+++ src/modelo/Novedades.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author ISABELLA CARMONA C
+ */
+public class Novedades {
+
+    private int idNovedades;
+    private String tipoid;
+    private String tipoNovedad;
+    private String descripcion;
+    private Date fechaNovedad;
+    private int idEmpleado;
+
+    public Novedades() {
+    }
+
+    public Novedades(int idNovedades, String tipoid, String tipoNovedad, String descripcion, java.sql.Date fechaNovedad, int idEmpleado) {
+        this.idNovedades = idNovedades;
+        this.tipoid = tipoid;
+        this.tipoNovedad = tipoNovedad;
+        this.descripcion = descripcion;
+        this.fechaNovedad = fechaNovedad;
+        this.idEmpleado = idEmpleado;
+    }
+
+    public int getIdNovedades() {
+        return idNovedades;
+    }
+
+    public void setIdNovedades(int idNovedades) {
+        this.idNovedades = idNovedades;
+    }
+
+    public String getTipoid() {
+        return tipoid;
+    }
+
+    public void setTipoid(String tipoid) {
+        this.tipoid = tipoid;
+    }
+
+    public String getTipoNovedad() {
+        return tipoNovedad;
+    }
+
+    public void setTipoNovedad(String tipoNovedad) {
+        this.tipoNovedad = tipoNovedad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public java.util.Date getFechaNovedad() {
+        return fechaNovedad;
+    }
+
+    public void setFechaNovedad(java.sql.Date fechaNovedad) {
+        this.fechaNovedad = fechaNovedad;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+}
