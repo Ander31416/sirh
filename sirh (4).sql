8367a5333e9f72fd967b81476d236367f00f7047
Ander31416 <89935076+Ander31416@users.noreply.github.com>
2022-02-12 13:41:22 -0600

    Add files via upload

---
+++ sirh (4).sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-02-2022 a las 20:41:03
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sirh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrador`
+--
+
+CREATE TABLE `administrador` (
+  `id` varchar(20) NOT NULL,
+  `user` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `Activate` varchar(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contactanos`
+--
+
+CREATE TABLE `contactanos` (
+  `Mensaje` text NOT NULL,
+  `TipoMensaje` varchar(20) NOT NULL,
+  `User` varchar(20) NOT NULL,
+  `FechaEnvio` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `tipoDocumento` varchar(40) NOT NULL,
+  `Cedula` varchar(10) NOT NULL,
+  `Nombres` varchar(40) DEFAULT NULL,
+  `Apellidos` varchar(40) DEFAULT NULL,
+  `Fecha_Nacimiento` date DEFAULT NULL,
+  `Telefono` varchar(10) DEFAULT NULL,
+  `Direccion` varchar(20) DEFAULT NULL,
+  `RH` varchar(3) DEFAULT NULL,
+  `EPS` varchar(30) DEFAULT NULL,
+  `ARL` varchar(30) DEFAULT NULL,
+  `SalarioBase` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `novedades`
+--
+
+CREATE TABLE `novedades` (
+  `Id_Novedades` varchar(20) NOT NULL,
+  `Descripcion` text DEFAULT NULL,
+  `Fecha_Novedad` date DEFAULT NULL,
+  `Id_Empleado` int(11) DEFAULT NULL,
+  `tipoNovedad` varchar(30) DEFAULT NULL,
+  `tipoDocumento` varchar(30) DEFAULT NULL,
+  `ArchivoNovedad` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `salario`
+--
+
+CREATE TABLE `salario` (
+  `Id_Salario` int(11) NOT NULL,
+  `Horas_Trabajadas` float DEFAULT NULL,
+  `Valor_Hora` float DEFAULT NULL,
+  `porc_ARL` float NOT NULL,
+  `porc_EPS` float NOT NULL,
+  `Id_Empleados` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `turnos`
+--
+
+CREATE TABLE `turnos` (
+  `Id_Turno` int(11) NOT NULL,
+  `Fecha_Inicio` date DEFAULT NULL,
+  `Fecha_Fin` date DEFAULT NULL,
+  `Hora_Inicio` time DEFAULT NULL,
+  `Hora_Fin` time DEFAULT NULL,
+  `Id_Empleado` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`Cedula`);
+
+--
+-- Indices de la tabla `novedades`
+--
+ALTER TABLE `novedades`
+  ADD PRIMARY KEY (`Id_Novedades`);
+
+--
+-- Indices de la tabla `salario`
+--
+ALTER TABLE `salario`
+  ADD PRIMARY KEY (`Id_Salario`),
+  ADD KEY `Id_Empleados` (`Id_Empleados`);
+
+--
+-- Indices de la tabla `turnos`
+--
+ALTER TABLE `turnos`
+  ADD PRIMARY KEY (`Id_Turno`),
+  ADD KEY `Id_Empleado` (`Id_Empleado`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `salario`
+--
+ALTER TABLE `salario`
+  MODIFY `Id_Salario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `turnos`
+--
+ALTER TABLE `turnos`
+  MODIFY `Id_Turno` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `salario`
+--
+ALTER TABLE `salario`
+  ADD CONSTRAINT `salario_ibfk_2` FOREIGN KEY (`Id_Empleados`) REFERENCES `empleados` (`Cedula`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `turnos`
+--
+ALTER TABLE `turnos`
+  ADD CONSTRAINT `turnos_ibfk_2` FOREIGN KEY (`Id_Empleado`) REFERENCES `empleados` (`Cedula`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
