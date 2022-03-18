a364ebb25adb8061efa8bb3056d7c87135704f2d
Ander31416 <89935076+Ander31416@users.noreply.github.com>
2022-03-18 08:42:54 -0500

    Update NovedadesDAO.java

--- src/modelo/NovedadesDAO.java
+++ src/modelo/NovedadesDAO.java
@@ -44,8 +44,8 @@ public class NovedadesDAO {
     
     public boolean GuardarNovedad(Novedades novedad) throws SQLException{
         String sql = "UPDATE `novedades` SET `Descripcion`='"+novedad.getDescripcion()+"',"
-                + "`Fecha_Novedad`='"+novedad.getFechaNovedad()+"',`Id_Empleado`='"+novedad.getIdEmpleado()+"',`tipoNovedad`='"+novedad.getTipoNovedad()+"',"
-                + "`tipoDocumento`='"+novedad.getTipoid()+"' WHERE `Id_Novedades` = '"+ ObtenerID().getIdNovedades() +"'";
+                + "`Fecha_Novedad_Inicial`='"+novedad.getFechaNovedadInicio()+"',`Fecha_Novedad_Final`='"+novedad.getFechaNovedadFin()+"',`Id_Empleado`='"+novedad.getIdEmpleado()+"',"
+                + "`tipoNovedad`='"+novedad.getTipoNovedad()+"',`tipoDocumento`='"+novedad.getTipoid()+"' WHERE `Id_Novedades` = '"+ ObtenerID().getIdNovedades() +"'";
        
         //Conectarse a la base de datos
         con = cn.getConnection();
@@ -75,7 +75,8 @@ public class NovedadesDAO {
             novedad.setIdNovedades(rs.getInt("Id_Novedades"));
             novedad.setTipoNovedad(rs.getString("tipoNovedad"));
             novedad.setDescripcion(rs.getString("Descripcion"));
-            novedad.setFechaNovedad(rs.getDate("Fecha_Novedad"));
+            novedad.setFechaNovedadInicio(rs.getDate("Fecha_Novedad_Inicial"));
+            novedad.setFechaNovedadFin(rs.getDate("Fecha_Novedad_Final"));
             novedad.setTipoid(rs.getString("tipoDocumento")); 
             novedad.setIdEmpleado(rs.getInt("Id_Empleado"));
         }
@@ -121,9 +122,9 @@ public class NovedadesDAO {
         return true;
     }
     
-    public boolean EditarFecha(Date fechasql, int Cod) {
+    public boolean EditarFechaInicio(Date fechasql, int Cod) {
         
-            String sql = "UPDATE `novedades` SET `Fecha_Novedad` = '"+ fechasql + "' WHERE `Id_Novedades` = '"+ Cod +"'";
+            String sql = "UPDATE `novedades` SET `Fecha_Novedad_Inicial` = '"+ fechasql + "' WHERE `Id_Novedades` = '"+ Cod +"'";
             
             //Conectarse a la base de datos
             con = cn.getConnection();
@@ -141,27 +142,28 @@ public class NovedadesDAO {
         return true;
     }
     
-    public int ContarArchivos() throws SQLException{
-        String sql = "SELECT COUNT(Id_Novedades) FROM novedades WHERE Descripcion is null";
+    public boolean EditarFechaFin(Date fechasql, int Cod) {
+        
+            String sql = "UPDATE `novedades` SET `Fecha_Novedad_Final` = '"+ fechasql + "' WHERE `Id_Novedades` = '"+ Cod +"'";
             
             //Conectarse a la base de datos
             con = cn.getConnection();
 
             try {
                 ps = con.prepareStatement(sql); //Envia la instruccion en comando sql
-                rs = ps.executeQuery(); //Ejecuta la instruccion
+                ps.executeUpdate(); //Ejecuta la instruccion
             } catch (SQLException ex) {
                 //Muestra el error en caso de haberlo
                 Logger.getLogger(NovedadesDAO.class.getName()).log(Level.SEVERE, null, ex);
-                System.out.println(ex);                
+                System.out.println(ex);
+                return false;
             }
-            
-        return rs.getInt("COUNT(Id_Novedades)");
+               
+        return true;
     }
     
-    public void EditarArchivo(int Id_Novedades, int ContarArchivos) throws SQLException{
+    public void EditarArchivo(int Id_Novedades) throws SQLException{
 
-            if(ContarArchivos != 0){
                 String sql = "SELECT * FROM novedades WHERE Descripcion is null";
                 
                 //Conectarse a la base de datos
@@ -174,20 +176,26 @@ public class NovedadesDAO {
                     //Muestra el error en caso de haberlo
                     Logger.getLogger(NovedadesDAO.class.getName()).log(Level.SEVERE, null, ex);
                     System.out.println(ex);                
+                }    
+                
+                Blob Archivo = null;
+
+                while(rs.next()){ 
+                   Archivo = rs.getBlob("ArchivoNovedad");
                 }
                 
-                sql = "UPDATE `novedades` SET `ArchivoNovedad`= '"+rs.getBlob("ArchivoNovedad")+"' WHERE Id_Novedades = '"+Id_Novedades+"'";
+                sql = "UPDATE `novedades` SET `ArchivoNovedad`= '"+Archivo+"' WHERE Id_Novedades = '"+Id_Novedades+"'";
                 
                 try {
                     ps = con.prepareStatement(sql); //Envia la instruccion en comando sql
-                    rs = ps.executeQuery(); //Ejecuta la instruccion
+                    ps.executeUpdate(); //Ejecuta la instruccion
                 } catch (SQLException ex) {
                     //Muestra el error en caso de haberlo
                     Logger.getLogger(NovedadesDAO.class.getName()).log(Level.SEVERE, null, ex);
                     System.out.println(ex);              
                 }
                 
-                sql = "DELETE from `novedades` where `Id_Novedades` = '@'";
+                sql = "DELETE from `novedades` where Descripcion is null";
 
                 try {
                     ps = con.prepareStatement(sql); //Envia la instruccion en comando sql
@@ -197,7 +205,7 @@ public class NovedadesDAO {
                     Logger.getLogger(NovedadesDAO.class.getName()).log(Level.SEVERE, null, ex);
                     System.out.println(ex);
                 }    
-            }
+            
     }
     
     public boolean EliminarNovedad(int idNovedades) throws SQLException{
@@ -280,12 +288,12 @@ public class NovedadesDAO {
         
         ps = con.prepareStatement(sql);
         ps.executeQuery();
-        if (rs.next()) {
+        while(rs.next()) {
             Blob doc = rs.getBlob("`ArchivoNovedad`"); 
-            InputStream ls = doc.getBinaryStream();
+            InputStream is = doc.getBinaryStream();
             
-            File file = new File("Documento_incapacidad_"+Id_Novedades);
-            BufferedInputStream in = new BufferedInputStream(ls);
+            File file = new File("Documento_novedad_"+Id_Novedades);
+            BufferedInputStream in = new BufferedInputStream(is);
             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
             
             byte[] bytes = new byte[8000];
@@ -293,13 +301,13 @@ public class NovedadesDAO {
             int len = 0;
             
             while((len = in.read(bytes)) > 0){
-                  out.write(bytes, 0, len);
+                out.write(bytes, 0, len);
             }
             
             out.flush();
             out.close();
             in.close();
-        }
+        } 
     }
      
     public void EliminarFecha(){
@@ -330,7 +338,8 @@ public class NovedadesDAO {
             novedad.setIdNovedades(rs.getInt("Id_Novedades"));
             novedad.setTipoNovedad(rs.getString("tipoNovedad"));
             novedad.setDescripcion(rs.getString("Descripcion"));
-            novedad.setFechaNovedad(rs.getDate("Fecha_Novedad"));
+            novedad.setFechaNovedadInicio(rs.getDate("Fecha_Novedad_Inicial"));
+            novedad.setFechaNovedadFin(rs.getDate("Fecha_Novedad_Final"));
             novedad.setTipoid(rs.getString("tipoDocumento")); 
             novedad.setIdEmpleado(rs.getInt("Id_Empleado"));
         }
